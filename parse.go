9cebe24cfdfde3145c8813e897a0d5f147629027
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-08-07 20:26:51 -0500

    refactor: Refactor list and atom handling
    These sections of the `readFromTokens` function were big enough to be
    factored into their own functions `readFromTokens` is now
    `buildExpressionTree`.

--- parse.go
+++ parse.go
@@ -60,55 +60,62 @@ func tokenize(input string) []string {
 	return tokens
 }
 
-func readFromTokens(expr *Expr, tokens []string) {
-	if len(tokens) == 0 {
-		next := Expr{LIST, nil, nil, nil}
-		expr.next = &next
-		return
+func handleList(expr *Expr, tokens []string) {
+	right_paren_index := findRightParen(tokens)
+
+	if right_paren_index != 0 {
+		child := Expr{LIST, nil, nil, nil}
+		expr.child = &child
+		buildExpressionTree(&child, tokens[:right_paren_index])
 	}
 
+	if len(tokens) > right_paren_index+1 {
+		next := Expr{LIST, nil, nil, nil}
+		expr.next = &next
+		buildExpressionTree(&next, tokens[right_paren_index+1:])
+	}
+}
+
+func handleAtom(expr *Expr, token string, tokens []string) {
+	if len(token) > 0 && token[0] == '"' { // Handle Strings
+		expr.kind = STRING
+		expr.value = token[1 : len(token)-1]
+
+	} else { // Handle Numbers and Symbols
+		expr.kind = NUMBER
+		val, e := strconv.ParseFloat(token, 64)
+
+		expr.value = val
+
+		if e != nil {
+			expr.kind = SYMBOL
+			expr.value = token
+		}
+	}
+	if len(tokens) > 0 {
+		next := Expr{LIST, nil, nil, nil}
+		expr.next = &next
+		buildExpressionTree(&next, tokens)
+	}
+}
+
+func buildExpressionTree(expr *Expr, tokens []string) {
 	token := tokens[0]
 	tokens = tokens[1:]
 
-	if token == "(" { // Handle Lists
-		right_paren_index := findRightParen(tokens)
-		child := Expr{LIST, nil, nil, nil}
-		expr.child = &child
-		readFromTokens(&child, tokens[:right_paren_index])
-		if len(tokens) > right_paren_index+1 {
-			next := Expr{LIST, nil, nil, nil}
-			expr.next = &next
-			readFromTokens(&next, tokens[right_paren_index+1:])
-		}
+	if token == "(" {
+		handleList(expr, tokens)
 	} else if token == ")" {
 		panic("unexpected )")
 
-	} else { // Handle Atoms
-		if len(token) > 0 && token[0] == '"' { // Handle Strings
-			expr.kind = STRING
-			expr.value = token[1 : len(token)-1]
-
-		} else { // Handle Numbers and Symbols
-			expr.kind = NUMBER
-			val, e := strconv.ParseFloat(token, 64)
-
-			expr.value = val
-
-			if e != nil {
-				expr.kind = SYMBOL
-				expr.value = token
-			}
-		}
-		if len(tokens) > 0 {
-			next := Expr{LIST, nil, nil, nil}
-			expr.next = &next
-			readFromTokens(&next, tokens)
-		}
+	} else {
+		handleAtom(expr, token, tokens)
 	}
 }
 
 func parse(input string) *Expr {
 	exp := Expr{LIST, nil, nil, nil}
-	readFromTokens(&exp, tokenize(input))
+	tokens := tokenize(input)
+	buildExpressionTree(&exp, tokens)
 	return &exp
 }
